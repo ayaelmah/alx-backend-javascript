9c411ca4621359f007ddbf17523d8e081ce508a5
ayaelmah <ayaelmahraoui22@gmail.com>
2024-10-05 00:37:37 +0000

    promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,3 @@
+function getResponseFromApi(){
+	return new Promise(() => {});
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseAPI(success){
+	return new Promise((resolve,reject) => {
+		if (success){
+			resolve({
+				status: 200,
+				body: 'success',
+			});
+		} else {
+			reject (new Error('the fake API is not working currently'));
+		}
+	});
+}
